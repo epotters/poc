b11609d973843d6fb9ebe74447297db2b3d89382
Eelko Potters <epotters@abz.nl>
2015-02-04 18:40:46 +0100

    Reorganize poms and add some files tot the Cordova build (not working yet)

---
+++ poc-rest/src/main/java/poc/rest/config/WebAppInitializer.java
@@ -0,0 +1,26 @@
+package poc.rest.config;
+
+
+import poc.rest.Application;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration.Dynamic;
+
+
+/**
+ * Created by epotters on 15-12-2014.
+ */
+public class WebAppInitializer implements WebApplicationInitializer {
+  public void onStartup(ServletContext servletContext) throws ServletException {
+    AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+    ctx.register(Application.class);
+    ctx.setServletContext(servletContext);
+    Dynamic dynamic = servletContext.addServlet("dispatcher", new DispatcherServlet(ctx));
+    dynamic.addMapping("/");
+    dynamic.setLoadOnStartup(1);
+  }
+}
