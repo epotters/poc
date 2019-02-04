71ce2f35d2ea11ab2f67636794d4540abd9966f1
Eelko Potters <epotters@xs4all.nl>
2019-02-04 02:00:16 +0100

    Small config fixes, some formatting

--- poc-web-api/src/main/resources/import.sql
+++ poc-web-api/src/main/resources/import.sql
@@ -1,12 +1,10 @@
+
+
 -- People
-INSERT INTO person (birth_date, birth_place, first_name, gender, last_name, prefix, household_id)
-VALUES ('1967-03-23', 'Leiden', 'Eelko', 'MALE', 'Potters', NULL, NULL);
-INSERT INTO person (birth_date, birth_place, first_name, gender, last_name, prefix, household_id)
-VALUES ('1971-08-20', 'Hoevelaken', 'Jacobine', 'FEMALE', 'Bijkerk', NULL, NULL);
-INSERT INTO person (birth_date, birth_place, first_name, gender, last_name, prefix, household_id)
-VALUES ('2003-11-26', 'Utrecht', 'Milan', 'MALE', 'Potters', NULL, NULL);
-INSERT INTO person (birth_date, birth_place, first_name, gender, last_name, prefix, household_id)
-VALUES ('2007-09-26', 'Utrecht', 'Senna', 'FEMALE', 'Potters', NULL, NULL);
+INSERT INTO person (birth_date, birth_place, first_name, gender, last_name, prefix, household_id) VALUES ('1967-03-23', 'Leiden', 'Eelko', 'MALE', 'Potters', NULL, NULL);
+INSERT INTO person (birth_date, birth_place, first_name, gender, last_name, prefix, household_id) VALUES ('1971-08-20', 'Hoevelaken', 'Jacobine', 'FEMALE', 'Bijkerk', NULL, NULL);
+INSERT INTO person (birth_date, birth_place, first_name, gender, last_name, prefix, household_id) VALUES ('2003-11-26', 'Utrecht', 'Milan', 'MALE', 'Potters', NULL, NULL);
+INSERT INTO person (birth_date, birth_place, first_name, gender, last_name, prefix, household_id) VALUES ('2007-09-26', 'Utrecht', 'Senna', 'FEMALE', 'Potters', NULL, NULL);
 
 -- Organisations
 
