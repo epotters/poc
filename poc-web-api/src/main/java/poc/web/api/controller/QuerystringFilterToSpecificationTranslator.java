51fbefea089009b8937e9a881b10744848c2fa36
Eelko Potters <epotters@xs4all.nl>
2019-03-22 16:06:43 +0100

    Separate class for filter translator

---
+++ poc-web-api/src/main/java/poc/web/api/controller/QuerystringFilterToSpecificationTranslator.java
@@ -0,0 +1,54 @@
+package poc.web.api.controller;
+
+
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.springframework.data.jpa.domain.Specification;
+import poc.core.repository.specification.BaseSpecification;
+import poc.core.repository.specification.SearchCriteria;
+import poc.core.repository.specification.SearchOperation;
+import poc.core.repository.specification.SpecificationsBuilder;
+
+
+class QuerystringFilterToSpecificationTranslator<T> {
+
+  private static final String SEPARATOR = ",";
+
+  private final String operationSetExpr = "(" + String.join("|", SearchOperation.SIMPLE_OPERATION_SET) + ")";
+
+  private final Pattern pattern = Pattern.compile("(\\w+?)" + operationSetExpr + "(\\w+?)", Pattern.UNICODE_CHARACTER_CLASS);
+
+  private static final SearchCriteria ALWAYS_TRUE_CRITERIA = new SearchCriteria("1", SearchOperation.EQUALITY, "1");
+
+
+  public Specification<T> translate(String filterParam) {
+    SpecificationsBuilder<T> builder = new SpecificationsBuilder<>();
+    String[] labelValueParts = filterParam.split(SEPARATOR);
+    Matcher matcher;
+    String fieldName;
+    for (String labelValue : labelValueParts) {
+      if (!"".equals(labelValue)) {
+        matcher = pattern.matcher(labelValue);
+        if (matcher.find()) {
+          fieldName = matcher.group(1);
+          builder.with(fieldName, matcher.group(2), matcher.group(3) + SearchOperation.WILDCARD);
+        }
+      }
+    }
+    return builder.build();
+  }
+
+
+  // TODO: Add a unit test
+  public Specification<T> translate(List<String> filters) {
+    Specification<T> specs = new BaseSpecification<T>(ALWAYS_TRUE_CRITERIA);
+    for (String filter : filters) {
+      specs.or(translate(filter));
+    }
+    return specs;
+  }
+
+
+}
