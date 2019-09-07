2e7732436cee923bb1085e525e8d30e1e477c5d7
Eelko Potters <epotters@xs4all.nl>
2019-09-07 23:10:38 +0200

    Prepare for animated navigation in editor

--- poc-web-gui-angular/poc-angular/src/app/organizations/organization-list-of-cards.component.ts
+++ poc-web-gui-angular/poc-angular/src/app/organizations/organization-list-of-cards.component.ts
@@ -6,11 +6,23 @@ import {MatDialog} from "@angular/material";
 import {OrganizationService} from "./organization.service";
 import {organizationMeta} from "./organization-meta";
 import {EntityListOfCardsComponent} from "../lib/entity-module/entity-list-of-cards.component";
+import {animate, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'organization-list-of-cards',
   templateUrl: './organization-list-of-cards.component.html',
-  styleUrls: ['../lib/entity-module/entity-list.component.css']
+  styleUrls: ['../lib/entity-module/entity-list.component.css'],
+  animations: [
+    trigger('slideInOut', [
+      transition(':enter', [
+        style({transform: 'translateY(-100%)'}),
+        animate('200ms ease-in', style({transform: 'translateY(0%)'}))
+      ]),
+      transition(':leave', [
+        animate('200ms ease-in', style({transform: 'translateY(-100%)'}))
+      ])
+    ])
+  ]
 })
 export class OrganizationListOfCardsComponent extends EntityListOfCardsComponent<Organization> {
 
@@ -24,6 +36,12 @@ export class OrganizationListOfCardsComponent extends EntityListOfCardsComponent
   ) {
 
     super(organizationMeta, service, router, route, dialog);
+  }
+
+
+  showEvent($event) {
+    console.debug('PageEvent received:');
+    console.debug($event);
   }
 
 }
