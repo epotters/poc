637dddbcab124f58fe59ed7cadb671318b62442e
Eelko Potters <epotters@xs4all.nl>
2019-11-01 13:52:34 +0100

    Move filter processing to a separate helper class

---
+++ poc-web-gui-angular/poc-angular/src/app/lib/entity-module/common/filter-builder.ts
@@ -0,0 +1,60 @@
+import {FieldFilter} from "../domain/filter.model";
+import {Moment} from "moment";
+import {EntityLibConfig} from "./entity-lib-config";
+import {ColumnConfig, EntityMeta} from "..";
+
+export class FilterBuilder<T extends Identifiable>{
+
+  exactMatchOperator = ':';
+  likeOperator = '~';
+
+  constructor(public meta: EntityMeta<T>) {
+  }
+
+  public buildFilterParams(filters?: FieldFilter[]): string {
+
+    let filterParams: string[] = [];
+    if (!filters || filters.length == 0) {
+      return '';
+    }
+
+    for (let filter of filters) {
+      let operator: string = this.likeOperator;
+      let value: string = filter.rawValue;
+      let editorType: string = this.getEditorType(filter.name);
+      if (editorType == 'select') {
+        operator = this.exactMatchOperator;
+      } else if (editorType == 'date') {
+        operator = this.exactMatchOperator;
+        value = (<Moment>(<any>filter.rawValue)).format(EntityLibConfig.dateFormat);
+      } else if (editorType === 'autocomplete') {
+        operator = this.exactMatchOperator;
+      } else if (filter.name === 'id' || filter.name.endsWith('.id')) {
+        operator = this.exactMatchOperator;
+      }
+      filterParams.push(filter.name + operator + value);
+    }
+    return filterParams.join(',');
+  }
+
+  private getEditorType(fieldName: string): string {
+    let columnConfig: ColumnConfig = this.meta.columnConfigs[fieldName];
+    if (fieldName.includes('.')) {
+      console.debug('Processing nested field', fieldName, this.meta.displayName);
+
+      let fieldNameParts: string[] = fieldName.split('.');
+      if (fieldNameParts.length != 2) {
+        console.warn(this.meta.displayName, 'Only one level of nesting supported', fieldName);
+        return null;
+      }
+      columnConfig = this.meta.columnConfigs[fieldNameParts[0]];
+      console.debug('fieldName', this.meta.columnConfigs, fieldNameParts[0], columnConfig);
+    }
+
+    if (!columnConfig.editor) {
+      return 'text';
+    }
+    return (columnConfig.editor.type ? columnConfig.editor.type : columnConfig.filter.type);
+  }
+
+}
