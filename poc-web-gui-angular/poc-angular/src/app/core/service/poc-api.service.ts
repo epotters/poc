8fc7800d899cb9fd262558636cd59a1865f381df
Eelko Potters <epotters@xs4all.nl>
2019-11-04 01:58:53 +0100

    Remove rsjx-compat, remove the last 'rxjs/Rx' import

--- poc-web-gui-angular/poc-angular/src/app/core/service/poc-api.service.ts
+++ poc-web-gui-angular/poc-angular/src/app/core/service/poc-api.service.ts
@@ -1,7 +1,6 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
 
-import 'rxjs/Rx';
 import {BehaviorSubject, Observable, throwError} from 'rxjs';
 import {catchError} from 'rxjs/operators';
 
