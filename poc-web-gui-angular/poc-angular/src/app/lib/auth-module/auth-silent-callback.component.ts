5596c1fdce6c3ab65c5000112213f49b218b480f
Eelko Potters <epotters@xs4all.nl>
2020-03-04 22:05:23 +0100

    angular-gui: detect if the silent c allback page is loaded in an iframe

--- poc-web-gui-angular/poc-angular/src/app/lib/auth-module/auth-silent-callback.component.ts
+++ poc-web-gui-angular/poc-angular/src/app/lib/auth-module/auth-silent-callback.component.ts
@@ -9,7 +9,7 @@ import {ActivatedRoute} from "@angular/router";
 })
 export class AuthSilentCallbackComponent implements OnInit {
 
-  message: string = 'Initializing Authentication Silent Callback Page';
+  message: string = 'Initializing Silent Authentication Callback Page';
 
   constructor(
     private authService: AuthService,
@@ -20,16 +20,27 @@ export class AuthSilentCallbackComponent implements OnInit {
   ngOnInit() {
     console.debug(this.message, location.href);
 
+    if (AuthSilentCallbackComponent.iniFrame()) {
+      console.debug("The Silent Authentication Callback Page is loaded in an iFrame");
+    } else {
+      console.debug("The Silent Authentication Callback Page is NOT loaded in an iFrame");
+    }
+
     // TODO: Handle error in the callback url
     let error: string | null = this.route.snapshot.queryParamMap.get('error');
     if (!!error && error === 'login_required') {
       console.debug('Error reported in silent auth callback url: ' + error);
     }
 
-
     this.authService.completeSilentAuthentication()
       .then(() => console.info('Silent refresh completed'))
       .catch((errorMessage: string) => console.error('Error while completing silent authentication: ' + errorMessage));
+  }
+
+
+  private static iniFrame(): boolean {
+    // return (window.self !== window.top);
+    return (window.location !== window.parent.location);
   }
 
 }
