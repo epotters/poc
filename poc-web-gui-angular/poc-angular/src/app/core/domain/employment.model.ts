2aa7c27cc1e6f07638b702bdd96a5ee38ccfd5f9
Eelko Potters <epotters@xs4all.nl>
2020-03-16 23:31:17 +0100

    poc-angular: use Moment as a type for date fields. Was string

--- poc-web-gui-angular/poc-angular/src/app/core/domain/employment.model.ts
+++ poc-web-gui-angular/poc-angular/src/app/core/domain/employment.model.ts
@@ -1,12 +1,13 @@
+import {Moment} from "moment";
+import {Identifiable} from "../../lib/entity-module";
 import {Organization} from "./organization.model";
 import {Person} from "./person.model";
-import {Identifiable} from "../../lib/entity-module";
 
 
 export class Employment implements Identifiable {
   id: number;
-  startDate: string;
-  endDate: string;
+  startDate: Moment;
+  endDate: Moment;
   employee: Person;
   employer: Organization;
   description: string;
--- poc-web-gui-angular/poc-angular/src/app/core/domain/person.model.ts
+++ poc-web-gui-angular/poc-angular/src/app/core/domain/person.model.ts
@@ -1,4 +1,6 @@
+import {Moment} from "moment";
 import {Identifiable} from "../../lib/entity-module";
+
 
 export class Person implements Identifiable {
   id: number;
@@ -6,11 +8,11 @@ export class Person implements Identifiable {
   prefix: string;
   lastName: string;
   gender: string;
-  birthDate: string;
+  birthDate: Moment;
   birthPlace: string;
   household: string;
 
   fullName(): string {
-    return this.firstName + ' ' + (this.prefix) ? this.prefix + ' ' : '' + this.lastName;
+    return `${this.firstName} ${(this.prefix) ? this.prefix + ' ' : ''}${this.lastName}`;
   }
 }
