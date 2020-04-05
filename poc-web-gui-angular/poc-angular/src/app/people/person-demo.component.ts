35a44ade5a51f650c1b3d1e6b2144a67f5fa2d1d
Eelko Potters <epotters@xs4all.nl>
2020-04-05 12:05:32 +0200

    poc-web-gui: split off the demo part in its own component from the person-manager and provide a link to it

---
+++ poc-web-gui-angular/poc-angular/src/app/people/person-demo.component.ts
@@ -0,0 +1,103 @@
+import {Component, ComponentFactoryResolver, OnInit, Type} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup} from '@angular/forms';
+import {MatDialog} from '@angular/material/dialog';
+import {ActivatedRoute} from '@angular/router';
+import {BehaviorSubject} from 'rxjs';
+
+import {PocAnimations} from '../app-animations';
+
+import {Person} from '../core/domain/';
+import {EntityManagerComponent} from '../lib/entity-lib';
+import {EntityComponentDescriptor} from '../lib/entity-lib/common/component-loader/component-loader';
+import {FieldFilter} from '../lib/entity-lib/domain/filter.model';
+import {PersonEditorComponent} from './person-editor.component';
+import {PersonListComponent} from './person-list.component';
+import {personMeta} from './person-meta';
+import {PersonService} from './person.service';
+
+
+@Component({
+  selector: 'person-demo',
+  templateUrl: './person-demo.component.html',
+  styleUrls: [],
+  animations: [
+    PocAnimations.fadeInOut,
+    PocAnimations.slideInOut
+  ]
+})
+export class PersonDemoComponent extends EntityManagerComponent<Person> implements OnInit {
+
+  demoFormVisible: boolean = true;
+
+  entityForm: FormGroup;
+  listComponent: Type<any> = PersonListComponent;
+  editorComponent: Type<any> = PersonEditorComponent;
+
+  entitySubject: BehaviorSubject<Person> = new BehaviorSubject<Person>({} as Person);
+
+  initialFilters: FieldFilter[];
+
+  constructor(
+    public service: PersonService,
+    public route: ActivatedRoute,
+    public dialog: MatDialog,
+    public componentFactoryResolver: ComponentFactoryResolver,
+    private formBuilder: FormBuilder
+  ) {
+    super(personMeta, service, route, dialog, componentFactoryResolver);
+
+    this.title = `Demo componenten ${this.meta.displayNamePlural.toLowerCase()}`;
+    this.initialFilters = [
+      {name: 'lastName', rawValue: 'po'}
+    ];
+
+    this.entityForm = this.buildSelectDemoForm(this.formBuilder);
+  }
+
+  ngOnInit() {
+
+    const person: Person = JSON.parse('{ "id": 350, "firstName": "Berriona", "prefix": null, "lastName": "Abbotts", "gender": "FEMALE", "birthDate": "2019-01-14", "birthPlace": "Thabazimbi", "birthCountry": null, "household": null }');
+    this.entitySubject.next(person);
+  }
+
+  buildSelectDemoForm(formBuilder: FormBuilder): FormGroup {
+    const group = {};
+    group['entitySelector'] = new FormControl('');
+    group['entitySelectorList'] = new FormControl('');
+
+    group['lastName'] = new FormControl('');
+    group['gender'] = new FormControl('');
+
+    return formBuilder.group(group);
+  }
+
+
+  toggleDemoForm() {
+    this.demoFormVisible = !this.demoFormVisible;
+  }
+
+
+  openDialogWithList() {
+    const entityListComponentDescriptor = new EntityComponentDescriptor(this.listComponent,
+      {
+        columns: this.meta.displayedColumnsDialog,
+        title: 'Minimal list with all features hidden',
+        headerVisible: false,
+        paginatorVisible: false,
+        filterVisible: false,
+        editorVisible: false,
+        initialFilters: this.initialFilters
+      });
+    this.openDialogWithEntityComponent(entityListComponentDescriptor);
+  }
+
+
+  openDialogWithEditor(entity?: Person) {
+    const entityEditorComponentDescriptor = new EntityComponentDescriptor(this.editorComponent, {
+      entityToLoad: entity,
+      isManaged: true
+    });
+    this.openDialogWithEntityComponent(entityEditorComponentDescriptor);
+  }
+
+}
