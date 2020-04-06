3bfd68b009cba0e3b7794d99734bdf783b9dda76
Eelko Potters <epotters@xs4all.nl>
2020-04-06 14:58:40 +0200

    poc-web-gui: Correct import environment in main.ts, import entity-lib styles in styles.css, add preserveSymlinks to angular.json to prevent a compilation error, Add auth-lib and entity-lib to package.json

--- poc-web-gui-angular/poc-angular/src/main.ts
+++ poc-web-gui-angular/poc-angular/src/main.ts
@@ -2,7 +2,7 @@ import {enableProdMode} from '@angular/core';
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 
 import {AppModule} from './app/app.module';
-import {Environment} from './environments/environment';
+import {Environment} from './environments';
 
 
 if (Environment.production) {
