4712d76f142eb7260c7d5ce39ce16961002d1dce
Eelko Potters <epotters@xs4all.nl>
2020-05-28 13:13:33 +0200

    poc-web-api: Add SecurityExceptionHandler to enable zalando problems for security errors

---
+++ poc-web-api/src/main/java/poc/web/api/controller/common/SecurityExceptionHandler.java
@@ -0,0 +1,10 @@
+package poc.web.api.controller.common;
+
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.zalando.problem.spring.web.advice.security.SecurityAdviceTrait;
+
+
+@ControllerAdvice
+public class SecurityExceptionHandler implements SecurityAdviceTrait {
+}
