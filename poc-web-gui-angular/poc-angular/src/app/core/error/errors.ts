462b65080a46371b752563537b10c0950c856e24
Eelko Potters <epotters@xs4all.nl>
2020-06-26 01:02:18 +0200

    poc-angular: Small enhancements, mostly cosmetic

--- poc-web-gui-angular/poc-angular/src/app/core/error/errors.ts
+++ poc-web-gui-angular/poc-angular/src/app/core/error/errors.ts
@@ -10,25 +10,25 @@ Source: https://openid.net/specs/openid-connect-core-1_0.html#AuthError
  */
 export class Errors {
 
-  static interaction_required: PocError = {
+  static INTERACTION_REQUIRED: PocError = {
     code: 'interaction_required',
     message: 'The Authorization Server requires End-User interaction of some form to proceed.\n' +
       'This error MAY be returned when the prompt parameter value in the Authentication Request is none, but the Authentication Request cannot be completed without displaying a user interface for End-User interaction.'
   };
 
-  static login_required: PocError = {
+  static LOGIN_REQUIRED: PocError = {
     code: 'login_required',
     message: 'The Authorization Server requires End-User authentication.\n' +
       'This error MAY be returned when the prompt parameter value in the Authentication Request is none, but the Authentication Request cannot be completed without displaying a user interface for End-User authentication.'
   };
 
-  static account_selection_required: PocError = {
+  static ACCOUNT_SELECTION_REQUIRED: PocError = {
     code: 'account_selection_required',
     message: 'The End-User is REQUIRED to select a session at the Authorization Server. The End-User MAY be authenticated at the Authorization Server with different associated accounts, but the End-User did not select a session.\n' +
       'This error MAY be returned when the prompt parameter value in the Authentication Request is none, but the Authentication Request cannot be completed without displaying a user interface to prompt for a session to use.'
   };
 
-  static consent_required: PocError = {
+  static CONSENT_REQUIRED: PocError = {
     code: 'consent_required',
     message: 'The Authorization Server requires End-User consent.\n' +
       'This error MAY be returned when the prompt parameter value in the Authentication Request is none, but the Authentication Request cannot be completed without displaying a user interface for End-User consent.'
